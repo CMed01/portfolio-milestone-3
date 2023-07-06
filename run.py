6f79cb399c1cedac2905a3cd5d4f6c6b469c5328
Christopher Meddings <c.m.meddings@gmail.com>
2023-07-06 15:57:50 +0000

    Print row number for game border

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+class Board:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+    
+
+def display_grid(height, width):
+    """
+    Builds game border and displays to terminal
+    """
+    print("\t\t Sweep the mines")
+    
+    empty_cell = "    "
+    for h in range(height):
+        row_border = empty_cell + str(h + 1)
+        print(row_border)
+
+    for w in range(width):
+        column_border = empty_cell + str(h + 1)
+        print(column_border)
+
+    print()
+
+display_grid(5, 4)
