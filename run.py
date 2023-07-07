e778f3fb6b096ba8d776ddb449884284f5a879c6
Christopher Meddings <c.m.meddings@gmail.com>
2023-07-07 16:58:57 +0000

    Create game grid for minesweeper game and add new game title

--- run.py
+++ run.py
@@ -8,21 +8,57 @@ class Board:
         self.width = width
     
 
-def display_grid(height, width):
+def display_grid(width):
     """
     Builds game border and displays to terminal
-    """
-    print("\t\t Sweep the mines")
-    
-    empty_cell = "    "
-    for h in range(height):
-        row_border = empty_cell + str(h + 1)
-        print(row_border)
+    Header row contains numbers using the width parameter
+    Remaining game grid consists of three rows that are repeated
+    Using for loop in a for loop
+    on both row and columne as per the width parameter:
+       
+    1st row =    |     |
+    2nd row =  1 |  ?  |
+    3rd row =    |_____|
 
-    for w in range(width):
-        column_border = empty_cell + str(h + 1)
-        print(column_border)
+    ? = mine_value
+    """
+    print("\t\t ChrisSweeper: Mine Hunter\n")
+
+    # Prints header row from the width paraeter
+    # Starting at number 1
+    row = "   "
+    for row_header in range(width):
+        row = row + "     " + str(row_header + 1)
+    
+    print(row)
+
+    # Prints the remainder of the game grid
+    for row_std in range(width):
+        # Prints the bottom header border
+        row = "    "
+        if row_std == 0:
+            for column in range(width):
+                row = row + "______"
+            print(row)
+
+        # prints the 1st row border
+        row = "    |"
+        for column in range(width):
+            row = row + "     |"
+        print(row)
+
+        # prints second row
+        row = str(row_std + 1) + "   |"
+        for column in range(width):
+            row = row + "  M" + "  |"
+        print(row)
+
+        # prints third row
+        row = "    |"
+        for column in range(width):
+            row = row + "_____|"
+        print(row)
 
     print()
 
-display_grid(5, 4)
+display_grid(9)
